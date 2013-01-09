e11c2279c9b29c8000465f9bf3792c553f7116b1
Taylor Mock <tmock12@gmail.com>
2013-01-09 09:45:49 -0500

    Remove password and confirmation from attr_accessible

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,7 +1,6 @@
 class Provider < ActiveRecord::Base
   include Authem::User
 
-  attr_accessible :cerp_number, :city, :state, :email, :fax, :name,
-    :password, :password_confirmation, :phone, :state,
-    :street_address_1, :street_address_2, :website, :zip_code
+  attr_accessible :cerp_number, :city, :state, :email, :fax, :name, :phone,
+    :state, :street_address_1, :street_address_2, :website, :zip_code
 end
