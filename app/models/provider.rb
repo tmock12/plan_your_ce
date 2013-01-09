0f673e67ff7fbbaff07ada06c6e25df4e21711ad
Taylor Mock <tmock12@gmail.com>
2013-01-09 09:47:20 -0500

    Validate presence of required fields on Provider

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -3,4 +3,7 @@ class Provider < ActiveRecord::Base
 
   attr_accessible :cerp_number, :city, :state, :email, :fax, :name, :phone,
     :state, :street_address_1, :street_address_2, :website, :zip_code
+
+  validates_presence_of :name, :street_address_1, :city, :state, :zip_code,
+    :phone, :email, :cerp_number
 end
--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Provider do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    subject { Provider.new }
+    it { should have(1).errors_on(:name) }
+    it { should have(1).errors_on(:street_address_1) }
+    it { should have(1).errors_on(:city) }
+    it { should have(1).errors_on(:state) }
+    it { should have(1).errors_on(:zip_code) }
+    it { should have(1).errors_on(:phone) }
+    it { should have(2).errors_on(:email) }
+    it { should have(1).errors_on(:cerp_number) }
+  end
+
 end
