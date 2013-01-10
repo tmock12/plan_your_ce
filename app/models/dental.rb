ad49827f3d980cf097abb437a37f8d3a5e871190
Taylor Mock <tmock12@gmail.com>
2013-01-10 16:39:38 -0500

    Add dental categories and careers

---
+++ app/models/dental.rb
@@ -0,0 +1,47 @@
+class Dental
+  CATEGORIES = [
+    'Anesthesia, Oral Sedation and Pain Control',
+    'Biomedical Sciences',
+    'Cariology and Caries Management',
+    'Community Oral Health/Health Policy',
+    'Courses for Allied Dental Staff',
+    'Dental Materials/Biomaterials',
+    'Dentist Health and Wellness',
+    'Diagnosis and Treatment Planning',
+    'Emergency Disaster Preparedness/Forensics',
+    'Emerging Technologies in Dentistry',
+    'Endodontics',
+    'Esthetic Dentistry',
+    'Ethics and Law',
+    'Evidence-Based Dentistry',
+    'General Dentistry',
+    'Geriatric Dentistry',
+    'Implantology',
+    'Infection Control and Dental Waste Management',
+    'Medical Emergencies in the Office',
+    'Occlusion',
+    'Oral and Maxillofacial Surgery',
+    'Oral Health Communication and Literacy',
+    'Oral Pathology',
+    'Oral Radiology, Imaging and Photography',
+    'Orthodontics',
+    'Pediatric Dentistry',
+    'Periodontics',
+    'Pharmacology and Therapeutics',
+    'Practice Management and Risk Management',
+    'Preventive Dentistry',
+    'Prosthodontics',
+    'Regulatory Compliance',
+    'Restorative Dentistry',
+    'Special Care Dentistry',
+    'TMJ/TMD and MPD'
+  ]
+  CAREERS = [
+    'General Practitioner',
+    'Dentist',
+    'Specialist',
+    'Dental Lab Technician',
+    'Dental Assistant',
+    'Dental Hygienist'
+  ]
+end
