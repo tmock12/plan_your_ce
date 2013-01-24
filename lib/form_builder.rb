2e1f8a5cf39c8a565e336dbbb7d9a9eea57e8c99
Taylor Mock <tmock12@gmail.com>
2013-01-24 10:33:58 -0500

    Add better form error messages

--- lib/form_builder.rb
+++ lib/form_builder.rb
@@ -1,7 +1,26 @@
 class FormBuilder < ActionView::Helpers::FormBuilder
+  EMAIL_OPTIONS = {autocomplete: 'off', autocorrect: 'off'}
 
   def label(method, text = nil, options = {}, &block)
     text = (text || method.to_s.humanize.titleize)
+    unless object.nil?
+      errors = object.errors[method.to_sym]
+      text += " <span class=\"error\">#{errors.is_a?(Array) ? errors.first : errors}</span>" if errors
+    end
     super(method, text.html_safe, options, &block)
+  end
+
+  def email_field(name, options={})
+    options[:class] ||= 'error' if object.errors[name.to_sym].present?
+    super(name, EMAIL_OPTIONS.merge(options))
+  end
+
+  %w(phone text password).each do |field_type|
+    class_eval %Q{
+      def #{field_type}_field(name, options={})
+        (options[:class] ||= '') << ' error' if object.errors[name.to_sym].present?
+        super(name, options)
+      end
+    }
   end
 end
