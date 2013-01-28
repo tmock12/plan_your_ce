01b0f524eee1bdd1a23a053dd0cde885e1c01d96
Taylor Mock <tmock12@gmail.com>
2013-01-28 10:25:35 -0500

    Seed airport codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+File.read(File.join(Rails.root, "db/airportcodes.txt")).each_line do |line|
+  location = line.strip
+  code = line.split('-').last.strip
+  Airport.create!(location: location, iata_code: code)
+end
