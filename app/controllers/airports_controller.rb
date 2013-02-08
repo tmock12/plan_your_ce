b99c8eebc8fd1ea6979cb4717cd842bb721f5a33
Taylor Mock <tmock12@gmail.com>
2013-02-08 15:48:05 -0500

    Refactor airports autocomplete

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,7 +1,7 @@
 class AirportsController < ApplicationController
 
   def index
-    @airports = Airport.order(:title).where("title ilike ?", "%#{params[:term]}%")
-    render json: @airports.map(&:title)
+    @airports = Airport.order(:title).where("title ilike ?", "%#{params[:term]}%").pluck(:title)
+    render json: @airports
   end
 end
